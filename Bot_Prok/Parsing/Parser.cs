49f8955d0a33da2863fd9849e230a727a73175a6
egormin <neoman2014@gmail.com>
2017-07-18 00:25:19 +0300

    first commit

---
+++ Bot_Prok/Parsing/Parser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot_Prok.Parsing
+{
+    class Parser
+    {
+        public static string Start(string sss)
+        {
+
+            return "";
+        }
+    }
+}
